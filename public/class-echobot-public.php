e4e2f41e1bc832d80604ad9579dced577bda97f5
Francisco Palacios Chaves <fpalacioschaves@gmail.com>
2019-03-14 13:58:21 +0100

    first commit

---
+++ public/class-echobot-public.php
@@ -0,0 +1,281 @@
+<?php
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/public
+ */
+
+/**
+ * The public-facing functionality of the plugin.
+ *
+ * Defines the plugin name, version, and two examples hooks for how to
+ * enqueue the public-facing stylesheet and JavaScript.
+ *
+ * @package    Plugin_Name
+ * @subpackage Plugin_Name/public
+ * @author     Your Name <email@example.com>
+ */
+class Echobot_Public {
+
+	/**
+	 * The ID of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $plugin_name    The ID of this plugin.
+	 */
+	private $plugin_name;
+
+	/**
+	 * The version of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 * @var      string    $version    The current version of this plugin.
+	 */
+	private $version;
+
+	/**
+	 * Initialize the class and set its properties.
+	 *
+	 * @since    1.0.0
+	 * @param      string    $plugin_name       The name of the plugin.
+	 * @param      string    $version    The version of this plugin.
+	 */
+	public function __construct( $plugin_name, $version ) {
+
+		$this->plugin_name = $plugin_name;
+		$this->version = $version;
+
+		//include("TextRazor.php");
+
+		//TextRazorSettings::setApiKey('1d89829be26ad0975ecd39ff7e61a07415da6eb0e8f923285a5e1b7a');
+
+		//include("partials/example.php");
+
+		// For ask a question
+		add_action(
+			'wp_ajax_nopriv_echobot_parse_ask_question', array(
+				$this,
+				'echobot_parse_ask_question',
+			)
+		);
+
+		add_action(
+			'wp_ajax_echobot_parse_ask_question', array(
+				$this,
+				'echobot_parse_ask_question',
+			)
+		);
+
+		// For welcome question
+		add_action(
+			'wp_ajax_nopriv_echobot_welcome', array(
+				$this,
+				'echobot_welcome',
+			)
+		);
+
+		add_action(
+			'wp_ajax_echobot_welcome', array(
+				$this,
+				'echobot_welcome',
+			)
+		);
+
+		// For featured products
+		add_action(
+			'wp_ajax_nopriv_echobot_suggested_products', array(
+				$this,
+				'echobot_suggested_products',
+			)
+		);
+
+		add_action(
+			'wp_ajax_echobot_suggested_products', array(
+				$this,
+				'echobot_suggested_products',
+			)
+		);
+
+
+
+	}
+
+	/**
+	 * Register the stylesheets for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_styles() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Plugin_Name_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Plugin_Name_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/echobot-public.css', array(), $this->version, 'all' );
+
+		wp_enqueue_style( "convform_css", plugin_dir_url( __FILE__ ) . 'css/jquery.convform.css', array(), $this->version, 'all' );
+
+		wp_enqueue_style( "convform_demo_css", plugin_dir_url( __FILE__ ) . 'css/demo.css', array(), $this->version, 'all' );
+
+
+	}
+
+	/**
+	 * Register the JavaScript for the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function enqueue_scripts() {
+
+		/**
+		 * This function is provided for demonstration purposes only.
+		 *
+		 * An instance of this class should be passed to the run() function
+		 * defined in Plugin_Name_Loader as all of the hooks are defined
+		 * in that particular class.
+		 *
+		 * The Plugin_Name_Loader will then create the relationship
+		 * between the defined hooks and the functions defined in this
+		 * class.
+		 */
+
+		wp_enqueue_script( "jquery_js", plugin_dir_url( __FILE__ ) . 'js/jquery-1.12.3.min.js', array( 'jquery' ), $this->version, true );
+
+		wp_enqueue_script( "autosize_js", plugin_dir_url( __FILE__ ) . 'js/autosize.min.js', array( 'jquery' ), $this->version, true );
+
+		wp_enqueue_script( "convform_js", plugin_dir_url( __FILE__ ) . 'js/jquery.convform.js', array( 'jquery' ), $this->version, true );
+
+		wp_enqueue_script( "echobot-public-js", plugin_dir_url( __FILE__ ) . 'js/echobot-public.js', array( 'jquery' ), $this->version, true );
+
+		//$username = $this->echobot_is_logged();
+
+		wp_localize_script(
+			"echobot-public-js",
+			'PublicGlobal',
+			array(
+				'ajax_url' => admin_url('admin-ajax.php'),
+				//'username' => $username,
+			)
+		);
+
+
+	}
+
+	public function echobot_parse_ask_question(){
+
+		$answer = $_POST["answer"];
+
+		$answer_array = explode(" ",$answer);
+
+		// Opciones del array de palabras
+
+		// Está la palabra "see" como en "i want to see"
+
+		// Está la palabra "buy" como en "i want to buy"
+
+
+		// a) Vamos a buscar si existe alguna página a la que se haga referencia desde la pregunta
+		$args = array(
+			'sort_order' => 'asc',
+			'sort_column' => 'post_title',
+			'hierarchical' => 0,
+			'parent' => -1,
+			'post_type' => 'page',
+			'post_status' => 'publish'
+		);
+		$response = array(
+			"title" =>"no_response"
+		);
+		$pages = get_pages($args);
+		foreach($pages as $page){
+
+			$pos = strpos(strtolower ($answer), strtolower ($page->post_name));
+			if ($pos !== false){
+				$response =  array(
+					"title"=>$page->post_title,
+					"url"=>get_page_link( $page->ID)
+				);
+			}
+		}
+
+
+		 echo json_encode($response);
+
+		wp_die();
+	}
+
+
+	public function echobot_welcome(){
+
+
+			ob_start();
+
+			include plugin_dir_path(__FILE__) . 'partials/echobot_welcome.php';
+
+			$welcome_message = ob_get_contents();
+
+			ob_end_clean();
+
+			echo $welcome_message;
+
+			wp_die();
+
+	}
+
+	public function echobot_suggested_products(){
+
+		// The tax query
+		$tax_query[] = array(
+			'taxonomy' => 'product_visibility',
+			'field'    => 'name',
+			'terms'    => 'featured',
+			'operator' => 'IN', // or 'NOT IN' to exclude feature products
+		);
+
+		// The query
+		$query = new WP_Query( array(
+			'post_type'           => 'product',
+			'post_status'         => 'publish',
+			'ignore_sticky_posts' => 1,
+			'posts_per_page'      => -1,
+			'orderby'             => "date",
+			'order'               => 'DESC',
+			'tax_query'           => $tax_query // <===
+		) );
+
+		var_dump($query);
+
+
+		ob_start();
+
+		include plugin_dir_path(__FILE__) . 'partials/echobot_featured.php';
+
+		$featured_message = ob_get_contents();
+
+		ob_end_clean();
+
+		echo $featured_message;
+
+		wp_die();
+
+	}
+
+
+
+
+}
---
+++ public/js/echobot-public.js
@@ -0,0 +1,200 @@
+(function( $ ) {
+	'use strict';
+
+	var logged = false;
+
+    var welcome_message
+
+    var convForm;
+
+    $(document).ready(function () {
+
+        // Welcome message
+        $.post(PublicGlobal.ajax_url,
+            {
+                action: "echobot_welcome",
+            })
+
+            .done(function (result) {
+                console.log(result)
+                welcome_message = result;
+                $("#content").append(welcome_message);
+                var count = 0;
+                convForm = $('#chat').convform({eventList:{onInputSubmit: function(convState, ready) {
+                            if(Array.isArray(convState.current.answer)) var answer = convState.current.answer.join(', ');
+                            else var answer = convState.current.answer.text;
+
+
+                            // Caso I want to ask you a question
+                            if(answer == "I want to ask you a question"){
+
+                                convState.current.next = convState.newState({
+                                    type: 'text',
+                                    name: 'dynamic-question-'+count,
+                                    questions: ['Ok guy, what is your question?' ],
+                                    noanswer:false,
+
+                                });
+                                var answer_ask_question = convState.current.answer.text;
+
+                                // Llamamos a una funcion en PHP que se encargue de trocear la respuesta y buscar dentro de las páginas
+                                // AJAX CALL
+                                $.post(PublicGlobal.ajax_url,
+                                    {
+                                        action: "echobot_parse_ask_question",
+                                        answer: answer_ask_question,
+                                    })
+
+                                    .done(function (result) {
+                                        console.log(result)
+                                        var result_array = jQuery.parseJSON(result);
+                                        if(result_array.title == "no_response"){
+                                            convState.current.next.next = convState.newState({
+                                                type: 'text',
+                                                name: 'dynamic-question-'+count,
+                                                questions: ['I dont understand you, guy. Can you repeat your question?' ],
+
+                                            });
+
+                                        }
+                                        else{
+                                            convState.current.next.next = convState.newState({
+                                                type: 'text',
+                                                noanswer: 'true',
+                                                name: 'dynamic-question-'+count,
+                                                questions: ['Please, follow me to the <a class=""button" href="'+result_array.url+'" target="_blank">' + result_array.title + ' </a>page' ],
+
+                                            });
+                                        }
+
+                                        //emulating random response time (100-600ms)
+                                        setTimeout(ready, Math.random()*500+100);
+
+                                    });
+                            }
+
+
+                            // Caso 2: Sugerencia. Podemos enviarle a los productos sugeridos
+                            else if(answer == "Make me any suggestion"){
+
+                                // AJAX CALL
+
+                                var answer_ask_question = convState.current.answer.text;
+
+                                $.post(PublicGlobal.ajax_url,
+                                    {
+                                        action: "echobot_suggested_products",
+                                    })
+
+                                    .done(function (result) {
+                                        console.log(result)
+                                        var result_array = jQuery.parseJSON(result);
+                                        if(result_array.title == "no_response"){
+                                            convState.current.next = convState.newState({
+                                                type: 'text',
+                                                name: 'dynamic-question-'+count,
+                                                questions: ['Sorry. we dont have featured products right now.' ],
+                                                noanswer: 'true',
+
+                                            });
+
+                                        }
+                                        else{
+                                            convState.current.next = convState.newState({
+                                                type: 'text',
+                                                noanswer: 'true',
+                                                name: 'dynamic-question-'+count,
+                                                questions: ['Please, follow me to the <a class=""button" href="'+result_array.url+'" target="_blank">' + result_array.title + ' </a>page' ],
+
+                                            });
+                                        }
+
+                                        setTimeout(ready, Math.random()*500+100);
+
+                                    });
+
+                            }
+
+                            // Caso 3. Le preguntamos por qué producto o tipo de producto quiere
+                            else if(answer == "Im looking for a product"){
+
+                            }
+
+                            // Caso 4: lo mandamos a los servicios de la empresa
+                            else if(answer == "I want info about the company"){
+
+                            }
+
+                            // Volver al inicio y refrescar el chat
+                            else if(answer.toLowerCase() == "refresh"){
+
+                                convState.current.next = false;
+                                //emulating random response time (100-600ms)
+                                setTimeout(ready, Math.random()*500+100);
+
+                            }
+
+
+                            // Estoy fuera de las opciones iniciales, busco en las páginas directamente
+                            else{
+                                // Llamamos a una funcion en PHP que se encargue de trocear la respuesta y buscar dentro de las páginas
+                                // AJAX CALL
+
+                                var answer_ask_question = convState.current.answer.text;
+
+                                $.post(PublicGlobal.ajax_url,
+                                    {
+                                        action: "echobot_parse_ask_question",
+                                        answer: answer_ask_question,
+                                    })
+
+                                    .done(function (result) {
+                                        console.log(result)
+                                        var result_array = jQuery.parseJSON(result);
+                                        if(result_array.title == "no_response"){
+                                            convState.current.next = convState.newState({
+                                                type: 'text',
+                                                name: 'dynamic-question-'+count,
+                                                questions: ['I dont understand you, guy. Can you repeat your question?' ],
+
+                                            });
+
+                                        }
+                                        else{
+                                            convState.current.next = convState.newState({
+                                                type: 'text',
+                                                noanswer: 'true',
+                                                name: 'dynamic-question-'+count,
+                                                questions: ['Please, follow me to the <a class=""button" href="'+result_array.url+'" target="_blank">' + result_array.title + ' </a>page' ],
+
+                                            });
+                                        }
+
+                                        setTimeout(ready, Math.random()*500+100);
+
+                                    });
+                            }
+
+                            count++;
+                        }}});
+
+            });
+
+    });
+
+    $(document).on("click", ".close-chat", function () {
+        $(".conv-form-wrapper").hide();
+        $(".card").css("width", "7%");
+        $(this).hide();
+        $(".open-chat").show();
+    });
+
+    $(document).on("click", ".open-chat", function () {
+        $(".conv-form-wrapper").show();
+        $(this).hide();
+        $(".close-chat").show();
+        $(".card").css("width", "25%");
+    });
+
+
+})( jQuery );
